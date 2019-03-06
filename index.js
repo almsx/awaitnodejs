06faacac47adfb4d9696b91fe80c4ed1f6aabcd6
Alberto Luebbert M <albertoluebbert@ideashappy.com>
2019-03-06 15:36:31 -0600

    Await example with conection Twitter API

---
+++ index.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+async function getNombre(username) {
+    const url = 'https://api.github.com/users/'+username;
+    const respuesta = await fetch(url)
+    const json = await respuesta.json()
+
+    if (respuesta.status !== 200){
+        throw Error('El usuario no existe');
+    }
+    return json.name;
+}
+
+(async function() {
+    try {
+        const nombre = await getNombre('almsx');
+        console.log(nombre);
+    } catch (e){
+        console.log(''+e);
+    }
+})()
